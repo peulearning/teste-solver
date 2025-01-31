0dc09ccd992d6eb44b2910fc0e0d40a21016b8bb
Pedro <peuhenry2018@gmail.com>
2025-01-31 19:57:29 -0300

    Carrinho Repository Adaptando ...

--- BackEndApp/Repositories/CarrinhoRepository.cs
+++ BackEndApp/Repositories/CarrinhoRepository.cs
@@ -1,8 +1,6 @@
-using BackendApp.Data;
 using BackendApp.Models;
+using BackendApp.Data;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace BackendApp.Repositories
 {
@@ -17,33 +15,36 @@ namespace BackendApp.Repositories
 
         public Carrinho GetById(int id)
         {
-            return _context.Carrinhos.Include(c => c.ItensCarrinho)
-                                     .FirstOrDefault(c => c.Id == id);
+            return _context.ItensCarrinhos
+                .Include(c => c.ItensCarrinho)  // Incluir itens do carrinho
+                .FirstOrDefault(c => c.Id == id);
         }
 
         public IEnumerable<Carrinho> GetAll()
         {
-            return _context.Carrinhos.Include(c => c.ItensCarrinho).ToList();
+            return _context.ItensCarrinhos
+                .Include(c => c.ItensCarrinho)  // Incluir itens do carrinho
+                .ToList();
         }
 
         public void Add(Carrinho carrinho)
         {
-            _context.Carrinhos.Add(carrinho);
+            _context.ItensCarrinhos.Add(carrinho);
             _context.SaveChanges();
         }
 
         public void Update(Carrinho carrinho)
         {
-            _context.Carrinhos.Update(carrinho);
+            _context.ItensCarrinhos.Update(carrinho);
             _context.SaveChanges();
         }
 
         public void Delete(int id)
         {
-            var carrinho = _context.Carrinhos.Find(id);
+            var carrinho = _context.ItensCarrinhos.Find(id);
             if (carrinho != null)
             {
-                _context.Carrinhos.Remove(carrinho);
+                _context.ItensCarrinhos.Remove(carrinho);
                 _context.SaveChanges();
             }
         }
